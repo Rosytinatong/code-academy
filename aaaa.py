4d66ac8b78d3b0aeafedc6b5e2cd747e571342c1
yutongchen <yutongc.mim2018@london.edu>
2018-08-29 16:23:00 -0700

    multiplecolour

--- aaaa.py
+++ aaaa.py
@@ -1,1 +1,14 @@
-a = 1
+// Color variables:
+red = [10, 100, 63];
+orange = [40, 100, 60];
+green = [75, 100, 40];
+blue = [196, 77, 55];
+purple = [280, 50, 60];
+
+// Letters in the message will cycle through these colors:
+letterColors = [orange, green, blue, purple, red];
+
+message = 'I love Salad';
+
+drawName(message, letterColors);
+bounceBubbles();
