a313c1052f1723123147cdd038c86540d8d64eeb
yutongchen <yutongc.mim2018@london.edu>
2018-08-29 16:30:08 -0700

    comment

--- Multiple Colors.js
+++ Multiple Colors.js
@@ -3,7 +3,7 @@ red = [10, 100, 63];
 orange = [40, 100, 60];
 green = [75, 100, 40];
 blue = [196, 77, 55];
-purple = [280, 50, 60];
+//purple = [280, 50, 60];
 
 // Letters in the message will cycle through these colors:
 letterColors = [orange, green, blue, purple, red];
